38562bd265d9b47d66ef16e54ae007eac0e51206
Vladyslav <grigovlad09112002@gmail.com>
2021-03-13 12:53:38 +0300

    UPD

--- nodemailer.js
+++ nodemailer.js
@@ -18,7 +18,7 @@ module.exports.sendMail = (data, result) => {
     }
 
     content = `   
-                    <table style=" border-collapse: collapse; border: 1px solid #afafaf; border-radius: 6px; max-width: 600px; width: 100%; padding-left: 15px; padding-right: 15px; margin: 0 auto">
+                    <table style=" border-collapse: collapse; border: 1px solid #afafaf; border-radius: 6px; max-width: 600px; width: 100%; padding-left: 15px; padding-right: 15px; padding-bottom: 80px; margin: 0 auto">
                             <tr>
                                 <td colspan="4" style="text-align: center"><h2>Заказ оформлен :)</h2></td>
                             </tr>
